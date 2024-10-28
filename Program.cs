328718efb808615e7af4f6198c103f1a2162e606
turbobeest <github@terbeest.com>
2024-10-28 13:13:58 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+
+namespace InaudibleAudioStreamer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Frequency above the typical human hearing range (20 kHz)
+            const double frequency = 19999.9;
+            const int sampleRate = 44100;
+            const float volume = 0.1f; // Low volume to prevent any possible artifacts
+
+            var signalGenerator = new SignalGenerator(sampleRate, 1)
+            {
+                Gain = volume,
+                Frequency = frequency,
+                Type = SignalGeneratorType.Sin
+            };
+
+            using (var waveOut = new WaveOutEvent())
+            {
+                waveOut.Init(signalGenerator);
+                waveOut.Play();
+
+                Console.WriteLine("Inaudible audio stream is now playing. Press Ctrl+C to exit.");
+
+                // Keep the application running
+                while (true)
+                {
+                    System.Threading.Thread.Sleep(1000);
+                }
+            }
+        }
+    }
+}
